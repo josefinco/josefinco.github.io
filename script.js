a5a74b08d50bd6230de0ca5134eab31e1be499f5
José Finco <josefinco_@hotmail.com>
2022-02-23 18:36:18 -0300

    Corrigir animação icones, adicionar icone rabbitMQ

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
+var elementos = document.querySelectorAll('.popup')
+let array = Array.from(elementos);
+
+
+array.forEach((item,index) => {
+  item.addEventListener('mouseenter', event => {
+    //TODO: ADICIONAR TRANSIÇÃO NO INDEX
+
+    if(index%2==0){
+      item.lastElementChild.classList.toggle("show");
+      console.log("PRA cima");
+    }else{
+      console.log("PRA BAIXO");
+      item.lastElementChild.classList.toggle("show");
+    }
+  })
+});
+
+array.forEach(item => {
+  item.addEventListener('mouseleave', event => {
+    item.lastElementChild.classList.remove("show");
+  })
+});
