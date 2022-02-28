4ac93030158a7e8c6dddee829fcd2edd4fbc091d
José Finco <josefinco_@hotmail.com>
2022-02-28 17:05:07 -0300

    Adicionar Javascript para animações

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-var root = document.documentElement;
-root.className += ' js';
-
-function boxTop(idBox) {
-  var boxOffset = $(idBox).offset().top;
-  return boxOffset;
-}
-
 var elementos = document.querySelectorAll('.popup')
 let array = Array.from(elementos);
 
@@ -29,28 +21,56 @@ array.forEach(item => {
   })
 });
 
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
 
-$(document).ready(function() {
-  var $target = $('.anime'),
-      animationClass = 'anime-init',
-      windowHeight = $(window).height(),
-      offset = windowHeight - (windowHeight / 4);
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+var windowPage = document.querySelector('.outer-wrapper')
 
-  function animeScroll() {
-    var documentTop = $(document).scrollTop();
-    $target.each(function() {
-      if (documentTop > boxTop(this) - offset) {
-        $(this).addClass(animationClass);
-      } else {
-        $(this).removeClass(animationClass);
+function animate() {
+
+  var parentPos = document.querySelector('.outer-wrapper').getBoundingClientRect(),
+  childPos = document.querySelector('.wrapper').getBoundingClientRect(),
+    relativePos = {};
+    relativePos.top = childPos.top - parentPos.top,
+    relativePos.right = childPos.right - parentPos.right,
+    relativePos.bottom = childPos.bottom - parentPos.bottom,
+    relativePos.left = childPos.left - parentPos.left;
+    const windowTop = Math.abs(relativePos.left)+ 500 ;
+
+    target.forEach(function(element){
+      childPos = document.querySelector('.wrapper').getBoundingClientRect(),
+      relativeTargetPos = {};
+      relativeTargetPos.top =  element.getBoundingClientRect().top - parentPos.top,
+      relativeTargetPos.right =  element.getBoundingClientRect().right - parentPos.right,
+      relativeTargetPos.bottom =  element.getBoundingClientRect().bottom - parentPos.bottom,
+      relativeTargetPos.left =  element.getBoundingClientRect().left - parentPos.left;
+      const targetTop = Math.abs(relativeTargetPos.left);
+      if (windowTop > (element.getBoundingClientRect().left - parentPos.left)){
+        element.classList.add(animationClass);
+      }else{
+        element.classList.remove(animationClass);
       }
-    });
-  }
-  animeScroll();
+    })
+}
 
-  $(document).scroll(function() {
-    animeScroll();
-  });
-});
+animate();
+
+windowPage.addEventListener('wheel', debounce(function(){
+  animate();
+}, 200));
 
 
